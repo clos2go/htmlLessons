4ceab98e1dc42f1b3f578e2e4d6baf94be9feb70
lopezlbj <carlos.lopez@austinisd.org>
2021-03-22 05:57:34 -0500

    audio stuffs 2

---
+++ 02_HTML5multimedia/03_stylingMediaPlayers/scripts.js
@@ -0,0 +1,16 @@
+let video;
+
+function init() {
+    // function  called when the page is loaded
+    video = document.querySelector('#myVideo')
+
+    //  for initial value
+    video.width = window.innerWidth;
+    video.height = window.innerHeight;
+
+    //  for dealing with window resize
+    window.onresize = function () {
+        video.width = window.innerWidth;
+        video.height = window.innerHeight;
+    }
+}
