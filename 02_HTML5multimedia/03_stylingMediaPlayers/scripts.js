9c92c3cee33f02fc354ec22bad1f544154d9ad58
lopezlbj <carlos.lopez@austinisd.org>
2021-03-24 08:38:49 -0500

    ????

--- 02_HTML5multimedia/03_stylingMediaPlayers/scripts.js
+++ 02_HTML5multimedia/03_stylingMediaPlayers/scripts.js
@@ -1,16 +1,40 @@
-let video;
+// let video;
 
-function init() {
-    // function  called when the page is loaded
-    video = document.querySelector('#myVideo')
+// function init() {
+//     // function  called when the page is loaded
+//     video = document.querySelector('#myVideo')
 
-    //  for initial value
-    video.width = window.innerWidth;
-    video.height = window.innerHeight;
+//     //  for initial value
+//     video.width = window.innerWidth;
+//     video.height = window.innerHeight;
 
-    //  for dealing with window resize
-    window.onresize = function () {
-        video.width = window.innerWidth;
-        video.height = window.innerHeight;
-    }
+//     //  for dealing with window resize
+//     window.onresize = function () {
+//         video.width = window.innerWidth;
+//         video.height = window.innerHeight;
+//     }
+// }
+
+function titleCase(title, minorWords) {
+  let newArr = [];
+  let tempWord = '';
+  let titleArr = title.split(' ');
+  let minorArr = minorWords.split(' ');
+
+
+  for (i = 0; i < titleArr.length; i++) {
+console.log(minorArr.includes(titleArr[i]))
+      for (j = 0; j < titleArr[i].length; j++) {
+        // console.log(title.arr[i])
+        
+        tempWord = titleArr[i].charAt(0).toUpperCase() + titleArr[i].slice(1).toLowerCase();
+         
+      }
+     console.log(tempWord);
+     newArr.push(tempWord)
+  }
+  console.log(newArr);
+
 }
+
+titleCase('a clash of KINGS', 'a an the of')
